32e7e20cb462c36322df8558b5b8545a9147aec8
Sergey Mazur <mazzurs@ya.ru>
2018-05-19 22:36:43 +0300

    Added profile, reg, and login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {MainComponent} from './main/main.component';
-import {RegComponent} from './reg/reg/reg.component';
+import {RegComponent} from './reg/reg.component';
+import {LoginComponent} from "./login/login.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MainComponent
-  },  {
-    path: 'reg',
-    component: RegComponent
-  }
+    {
+        path: '',
+        component: MainComponent
+    }, {
+        path: 'reg',
+        component: RegComponent
+    }, {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import {AppComponent} from './app.component';
 import {MainComponent} from './main/main.component';
 import {MaterialModule} from './material/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { RegComponent } from './reg/reg/reg.component';
+import { RegComponent } from './reg/reg.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    RegComponent
+    RegComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserAnimationsModule,
