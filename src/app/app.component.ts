9378764ac78379c74a6b19ea609f7dd711c4bc8e
Antony <Toxa23A@gmail.com>
2018-05-20 07:20:14 +0300

    ammend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   userName;
-  constructor(){
-    this.userName = JSON.parse(localStorage.getItem('user')).nickname;
+
+  constructor() {
+  }
+
+  ngOnInit(){
+    let user = JSON.parse(localStorage.getItem('user'))
+    if (user) {
+      this.userName = user.nickname;
+    }
   }
 }
